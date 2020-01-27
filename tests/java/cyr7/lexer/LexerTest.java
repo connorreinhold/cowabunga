91c9ce87454a4a51241130d7965e59d49bfa9770
William Ma <whoiswillma@gmail.com>
2020-01-26 22:39:13 -0500

    Remove junit jars

--- tests/java/cyr7/lexer/LexerTest.java
+++ tests/java/cyr7/lexer/LexerTest.java
@@ -1,18 +1,29 @@
 package cyr7.lexer;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.jupiter.api.Test;
 
+import java.io.FileReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.util.Scanner;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class LexerTest {
 
     @Test
-    void add() {
-        MyLexer lexer = new MyLexer(new InputStreamReader(getClass().getClassLoader().getResourceAsStream("add.xi")));
+    void add() throws IOException {
+        fail();
 
+        MyLexer lexer = new MyLexer(new InputStreamReader(getClass().getClassLoader().getResourceAsStream("add.xi")));
+        Scanner actualFile = new Scanner(getClass().getClassLoader().getResourceAsStream("add.lexedsol"));
+
+        MyLexer.Token token;
+        String line;
+        while ((token = lexer.nextToken()) != null && (line = actualFile.nextLine()) != null) {
+            assertEquals(token.toString(), line);
+        }
     }
 
 }
