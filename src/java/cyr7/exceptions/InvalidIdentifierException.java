bac5d452bb717dfd22293bc8a5a3f51c254e0259
vagrant <vagrant@cs4120>
2020-02-01 20:09:55 +0000

    Merge with exceptions branch. Add invalid token exception for characters with no meaning such as '#'

---
+++ src/java/cyr7/exceptions/InvalidIdentifierException.java
@@ -0,0 +1,14 @@
+package cyr7.exceptions;
+
+public class InvalidIdentifierException extends LexerException {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = -8216669670677839037L;
+    
+    public InvalidIdentifierException(String c, int line, int col) {
+	super(String.format("Invalid identifier token: %s", c), line, col);
+    }
+    
+}
