9f5968fe395a53756726469225dc06f20f8971a2
Anthony Yang <ay339@cornell.edu>
2020-02-02 14:51:38 -0500

    Add Multiline exceptions

--- src/java/cyr7/exceptions/LexerException.java
+++ src/java/cyr7/exceptions/LexerException.java
@@ -12,5 +12,4 @@ public class LexerException extends IOException {
     public LexerException(String msg, int line, int col) {
 	super(String.format("%d:%d error:%s", line + 1, col + 1, msg));
     }
-    
 }
