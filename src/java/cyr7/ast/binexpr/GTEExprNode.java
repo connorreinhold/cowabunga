72bdca346fc6ad4b99dec2a3c9ba5357e2613ef6
Orko Sinha <37596785+orkosinha@users.noreply.github.com>
2020-02-10 20:51:22 -0500

    Add boolean operations

---
+++ src/java/cyr7/ast/binexpr/GTEExprNode.java
@@ -0,0 +1,20 @@
+package cyr7.ast.binexpr;
+
+import cyr7.ast.ExprNode;
+import edu.cornell.cs.cs4120.util.SExpPrinter;
+
+public class GTEExprNode extends BinExprNode{
+
+    public GTEExprNode(ExprNode left, ExprNode right) {
+        super(left, right);
+    }
+
+    @Override
+    public void prettyPrint(SExpPrinter printer) {
+        printer.startList();
+        printer.printAtom(">=");
+        left.prettyPrint(printer);
+        right.prettyPrint(printer);
+        printer.endList();
+    }
+}
