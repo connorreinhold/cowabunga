ea1ed4990e19329b523169b5aa6bcf834c0d1938
William Ma <whoiswillma@gmail.com>
2020-02-13 09:47:50 -0500

    Remove VarDeclOrDiscardList production

--- src/java/cyr7/parser/ParserUtil.java
+++ src/java/cyr7/parser/ParserUtil.java
@@ -1,5 +1,9 @@
 package cyr7.parser;
 
+import cyr7.ast.INode;
+import edu.cornell.cs.cs4120.util.CodeWriterSExpPrinter;
+
+import java.io.PrintWriter;
 import java.io.Reader;
 import java.io.Writer;
 
@@ -7,6 +11,13 @@ public class ParserUtil {
 
     public static void parse(Reader reader, Writer writer) {
 
+    }
+
+    public static void printSExpr(INode node) {
+        CodeWriterSExpPrinter printer = new CodeWriterSExpPrinter(new PrintWriter(System.out));
+        node.prettyPrint(printer);
+        printer.flush();
+        printer.close();
     }
 
 }
