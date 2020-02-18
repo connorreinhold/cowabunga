3574d1a87d922d8dd9e35cc422c9616458a8f08e
Anthony Yang <ay339@cornell.edu>
2020-02-18 01:40:12 -0500

    Implement typecheeck for type nodes

--- src/java/cyr7/ast/type/PrimitiveTypeNode.java
+++ src/java/cyr7/ast/type/PrimitiveTypeNode.java
@@ -2,18 +2,21 @@ package cyr7.ast.type;
 
 import cyr7.ast.AbstractNode;
 import cyr7.ast.expr.unaryexpr.BoolNegExprNode;
+import cyr7.exceptions.SemanticException;
 import cyr7.semantics.Context;
+import cyr7.semantics.ExpandedType;
+import cyr7.semantics.OrdinaryType;
 import cyr7.semantics.PrimitiveType;
 import edu.cornell.cs.cs4120.util.SExpPrinter;
 import java_cup.runtime.ComplexSymbolFactory;
+import java_cup.runtime.ComplexSymbolFactory.Location;
 
-public class PrimitiveTypeNode extends AbstractNode implements ITypeExprNode {
+public class PrimitiveTypeNode extends ITypeExprNode {
 
     public final PrimitiveEnum type;
 
-    public PrimitiveTypeNode(ComplexSymbolFactory.Location location, PrimitiveEnum type) {
+    public PrimitiveTypeNode(Location location, PrimitiveEnum type) {
         super(location);
-
         this.type = type;
     }
 
@@ -28,5 +31,16 @@ public class PrimitiveTypeNode extends AbstractNode implements ITypeExprNode {
             return this.type.equals(oNode.type);
         }
         return false;
+    }
+
+    @Override
+    public OrdinaryType typeCheck(Context c) throws SemanticException {
+        switch (type) {
+        case BOOL:
+            return PrimitiveType.BOOL;
+        case INT:
+            return PrimitiveType.INT;
+        }
+        throw new SemanticException("Unexpected type check");
     }
 }
--- src/java/cyr7/ast/type/TypeExprArrayNode.java
+++ src/java/cyr7/ast/type/TypeExprArrayNode.java
@@ -3,11 +3,23 @@ package cyr7.ast.type;
 import cyr7.ast.AbstractNode;
 import cyr7.ast.FunctionHeaderDeclNode;
 import cyr7.ast.expr.ExprNode;
+import cyr7.exceptions.SemanticException;
+import cyr7.semantics.ArrayType;
 import cyr7.semantics.Context;
+import cyr7.semantics.ExpandedType;
+import cyr7.semantics.OrdinaryType;
+import cyr7.semantics.PrimitiveType;
 import edu.cornell.cs.cs4120.util.SExpPrinter;
 import java_cup.runtime.ComplexSymbolFactory;
+import java_cup.runtime.ComplexSymbolFactory.Location;
 
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.Optional;
+import java.util.Queue;
+import java.util.Stack;
+
+import com.sun.org.apache.bcel.internal.classfile.Node;
 
 /**
  * Represents an XI array type. Each TypeExprArrayNode contains an optional
@@ -19,12 +31,12 @@ import java.util.Optional;
  * TypeExprArrayNode(TypeExprArrayNode(PrimitiveEnumNode(int), Optional
  * .empty()), Optional.of(4))
  */
-public final class TypeExprArrayNode extends AbstractNode implements ITypeExprNode {
+public final class TypeExprArrayNode extends ITypeExprNode {
 
     public final ITypeExprNode child;
     public final Optional<ExprNode> size;
 
-    public TypeExprArrayNode(ComplexSymbolFactory.Location location,
+    public TypeExprArrayNode(Location location,
                              ITypeExprNode child, Optional<ExprNode> size) {
         super(location);
 
@@ -54,6 +66,12 @@ public final class TypeExprArrayNode extends AbstractNode implements ITypeExprNo
         }
         return false;
 
+    }
+
+    @Override
+    public OrdinaryType typeCheck(Context c) throws SemanticException {
+        OrdinaryType type = this.child.typeCheck(c);
+        return new ArrayType(type);
     }
 
 }
