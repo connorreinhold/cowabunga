b1e1d3d59984f2c1b05e919b2e66df1803980e37
Anthony Yang <ay339@cornell.edu>
2020-02-18 00:12:34 -0500

    Add typecheck for procedure statements

--- src/java/cyr7/ast/stmt/ProcedureStmtNode.java
+++ src/java/cyr7/ast/stmt/ProcedureStmtNode.java
@@ -1,17 +1,18 @@
 package cyr7.ast.stmt;
 
-import cyr7.ast.AbstractNode;
 import cyr7.ast.expr.FunctionCallExprNode;
 import cyr7.exceptions.SemanticException;
 import cyr7.exceptions.UnbalancedPushPopException;
 import cyr7.semantics.Context;
+import cyr7.semantics.ExpandedType;
 import cyr7.semantics.ResultType;
+import cyr7.semantics.UnitType;
 import edu.cornell.cs.cs4120.util.SExpPrinter;
 import java_cup.runtime.ComplexSymbolFactory;
 
 /**
- * Represents a call to a procedure [procedureCall], which is a FunctionCallExprNode with
- * no return types
+ * Represents a call to a procedure [procedureCall], which is a 
+ * FunctionCallExprNode with return type unit.
  */
 public class ProcedureStmtNode extends StmtNode {
 	public final FunctionCallExprNode procedureCall;
@@ -41,8 +42,13 @@ public class ProcedureStmtNode extends StmtNode {
     @Override
     public ResultType typeCheck(Context c) throws SemanticException,
             UnbalancedPushPopException {
-        // TODO Auto-generated method stub
-        return null;
+        ExpandedType type = procedureCall.typeCheck(c);
+        if (type.equals(UnitType.UNIT)) {
+            return ResultType.UNIT;
+        } else {
+            throw new SemanticException("Expected a procedure but found a "
+                    + "function instead.");
+        }
     }
 
 
