3e462dd6fcd2bc12876466ba9dec2b6851f982b1
Connor Reinhold <cyreinhold@gmail.com>
2020-02-17 22:27:21 -0500

    Pushing

--- src/java/cyr7/ast/expr/FunctionCallExprNode.java
+++ src/java/cyr7/ast/expr/FunctionCallExprNode.java
@@ -1,12 +1,17 @@
 package cyr7.ast.expr;
 
 import cyr7.ast.expr.ExprNode;
+import cyr7.exceptions.SemanticException;
+import cyr7.semantics.Context;
+import cyr7.semantics.ExpandedType;
+import cyr7.semantics.FunctionType;
+import cyr7.semantics.VariableType;
 import cyr7.util.Util;
 import edu.cornell.cs.cs4120.util.SExpPrinter;
 import java_cup.runtime.ComplexSymbolFactory;
 
-import java.util.LinkedList;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Represents a call to a function (which is still an expression). Contains the identifier of the
@@ -49,4 +54,13 @@ public class FunctionCallExprNode extends ExprNode {
         }
         return false;
     }
+
+	@Override
+	public ExpandedType typeCheck(Context c) throws SemanticException {
+		Optional<FunctionType> optionalVar = c.getFn(this.identifier);
+		if (optionalVar.isPresent()) {
+			return optionalVar.get();
+		}
+		return null;
+	}
 }
