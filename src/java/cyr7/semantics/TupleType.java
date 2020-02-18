644697a4da55d046c862ac1e3a0deb6f2e26c359
William Ma <whoiswillma@gmail.com>
2020-02-18 10:01:14 -0500

    Fix formatting

--- src/java/cyr7/semantics/TupleType.java
+++ src/java/cyr7/semantics/TupleType.java
@@ -4,25 +4,20 @@ import java.util.Collections;
 import java.util.List;
 
 public final class TupleType implements ExpandedType{
-	public final List<ExpandedType> elements;
-	
-	public TupleType(List<ExpandedType> elements) {
-	    assert(elements.size() >= 2);
-		this.elements = Collections.unmodifiableList(elements);
-	}
+    public final List<ExpandedType> elements;
 
-	@Override
-	public boolean equals(Object o) {
-	    if (o instanceof TupleType) {
-	        TupleType tupleType = (TupleType) o;
-	        return elements.equals(tupleType.elements);
-	    }
-	    return false;
-	}
+    public TupleType(List<ExpandedType> elements) {
+        assert(elements.size() >= 2);
+        this.elements = Collections.unmodifiableList(elements);
+    }
 
-	@Override
-	public int hashCode() {
-		return 0;
-	}
+    @Override
+    public boolean equals(Object o) {
+        if (o instanceof TupleType) {
+            TupleType tupleType = (TupleType) o;
+            return elements.equals(tupleType.elements);
+        }
+        return false;
+    }
 
 }
