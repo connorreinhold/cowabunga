9c78a950c54917309c834920490e7f5c5ba6dae5
Connor Reinhold <cyreinhold@gmail.com>
2020-03-02 18:36:13 -0500

    Beginning framework for tests

--- src/java/cyr7/ir/AstToIrVisitor.java
+++ src/java/cyr7/ir/AstToIrVisitor.java
@@ -44,7 +44,10 @@ import cyr7.ast.toplevel.UseNode;
 import cyr7.ast.toplevel.XiProgramNode;
 import cyr7.ast.type.PrimitiveTypeNode;
 import cyr7.ast.type.TypeExprArrayNode;
-import cyr7.ir.nodes.*;
+import cyr7.ir.nodes.IRBinOp;
+import cyr7.ir.nodes.IRConst;
+import cyr7.ir.nodes.IRExpr;
+import cyr7.ir.nodes.IRNode;
 import cyr7.visitor.AbstractVisitor;
 
 public class AstToIrVisitor extends AbstractVisitor<IRNode> {
@@ -164,84 +167,91 @@ public class AstToIrVisitor extends AbstractVisitor<IRNode> {
 
     @Override
     public IRNode visit(AddExprNode n) {
-        return new IRBinOp(IRBinOp.OpType.ADD, this.visit(n.left), this.visit(n.right));
+        return new IRBinOp(IRBinOp.OpType.ADD, this.visit(n.left),
+                this.visit(n.right));
     }
 
     @Override
     public IRNode visit(AndExprNode n) {
         /*
-        return new IRESeq(new IRSeq(
-                new IRMove(new IRTemp("f"), new IRConst(0)),
-                new IRCJump(),
-                new IRLabel(),
-                new IRCJump(),
-                new IRLabel(),
-                new IRMove(new IRTemp(), new IRConst(1)),
-                new IRLabel()),
-                new IRTemp()
-        ));
-        */
+         * return new IRESeq(new IRSeq( new IRMove(new IRTemp("f"), new
+         * IRConst(0)), new IRCJump(), new IRLabel(), new IRCJump(), new
+         * IRLabel(), new IRMove(new IRTemp(), new IRConst(1)), new IRLabel()),
+         * new IRTemp() ));
+         */
         return null;
     }
 
     @Override
     public IRNode visit(DivExprNode n) {
-        return new IRBinOp(IRBinOp.OpType.DIV, this.visit(n.left), this.visit(n.right));
+        return new IRBinOp(IRBinOp.OpType.DIV, this.visit(n.left),
+                this.visit(n.right));
     }
 
     @Override
     public IRNode visit(EqualsExprNode n) {
-        return new IRBinOp(IRBinOp.OpType.EQ, this.visit(n.left), this.visit(n.right));
+        return new IRBinOp(IRBinOp.OpType.EQ, this.visit(n.left),
+                this.visit(n.right));
     }
 
     @Override
     public IRNode visit(GTEExprNode n) {
-        return new IRBinOp(IRBinOp.OpType.GEQ, this.visit(n.left), this.visit(n.right));
+        return new IRBinOp(IRBinOp.OpType.GEQ, this.visit(n.left),
+                this.visit(n.right));
     }
 
     @Override
     public IRNode visit(GTExprNode n) {
-        return new IRBinOp(IRBinOp.OpType.GT, this.visit(n.left), this.visit(n.right));
+        return new IRBinOp(IRBinOp.OpType.GT, this.visit(n.left),
+                this.visit(n.right));
     }
 
     @Override
     public IRNode visit(HighMultExprNode n) {
-        return new IRBinOp(IRBinOp.OpType.HMUL, this.visit(n.left), this.visit(n.right));
+        return new IRBinOp(IRBinOp.OpType.HMUL, this.visit(n.left),
+                this.visit(n.right));
     }
 
     @Override
     public IRNode visit(LTEExprNode n) {
-        return new IRBinOp(IRBinOp.OpType.LEQ, this.visit(n.left), this.visit(n.right));
+        return new IRBinOp(IRBinOp.OpType.LEQ, this.visit(n.left),
+                this.visit(n.right));
     }
 
     @Override
     public IRNode visit(LTExprNode n) {
-        return new IRBinOp(IRBinOp.OpType.LT, this.visit(n.left), this.visit(n.right));
+        return new IRBinOp(IRBinOp.OpType.LT, this.visit(n.left),
+                this.visit(n.right));
     }
 
     @Override
     public IRNode visit(MultExprNode n) {
-        return new IRBinOp(IRBinOp.OpType.MUL, this.visit(n.left), this.visit(n.right));
+        return new IRBinOp(IRBinOp.OpType.MUL, this.visit(n.left),
+                this.visit(n.right));
     }
 
     @Override
     public IRNode visit(NotEqualsExprNode n) {
-        return new IRBinOp(IRBinOp.OpType.NEQ, this.visit(n.left), this.visit(n.right));
+        return new IRBinOp(IRBinOp.OpType.NEQ, this.visit(n.left),
+                this.visit(n.right));
     }
 
     @Override
     public IRNode visit(OrExprNode n) {
-        return new IRBinOp(IRBinOp.OpType.OR, this.visit(n.left), this.visit(n.right));
+        return new IRBinOp(IRBinOp.OpType.OR, this.visit(n.left),
+                this.visit(n.right));
     }
 
     @Override
     public IRNode visit(RemExprNode n) {
-        return new IRBinOp(IRBinOp.OpType.MOD, this.visit(n.left), this.visit(n.right));
+        return new IRBinOp(IRBinOp.OpType.MOD, this.visit(n.left),
+                this.visit(n.right));
     }
 
     @Override
     public IRNode visit(SubExprNode n) {
-        return new IRBinOp(IRBinOp.OpType.SUB, this.visit(n.left), this.visit(n.right));
+        return new IRBinOp(IRBinOp.OpType.SUB, this.visit(n.left),
+                this.visit(n.right));
     }
 
     @Override
@@ -251,7 +261,7 @@ public class AstToIrVisitor extends AbstractVisitor<IRNode> {
 
     @Override
     public IRNode visit(LiteralBoolExprNode n) {
-        return new IRConst(n.contents? 1:0);
+        return new IRConst(n.contents ? 1 : 0);
     }
 
     @Override
@@ -261,11 +271,13 @@ public class AstToIrVisitor extends AbstractVisitor<IRNode> {
 
     @Override
     public IRNode visit(LiteralIntExprNode n) {
-        return new IRConst(Integer.parseInt(n.contents));
+        return new IRConst(Long.parseLong(n.contents));
     }
 
     @Override
-    public IRNode visit(LiteralStringExprNode n) { return null; }
+    public IRNode visit(LiteralStringExprNode n) {
+        return null;
+    }
 
     @Override
     public IRNode visit(BoolNegExprNode n) {
---
+++ tests/java/cyr7/ir/util/IRFactory.java
@@ -0,0 +1,31 @@
+package cyr7.ir.util;
+
+import cyr7.ast.AbstractNode;
+import cyr7.ir.AstToIrVisitor;
+import cyr7.ir.interpret.IRSimulator;
+import cyr7.ir.nodes.IRCompUnit;
+import cyr7.ir.nodes.IRExpr;
+import cyr7.ir.nodes.IRFuncDecl;
+import cyr7.ir.nodes.IRNode;
+import cyr7.ir.nodes.IRReturn;
+import cyr7.ir.nodes.IRSeq;
+import cyr7.ir.nodes.IRStmt;
+
+public class IRFactory {
+    public static IRNode parseAst(AbstractNode astNode) {
+        AstToIrVisitor visitor = new AstToIrVisitor();
+        IRNode result = astNode.accept(visitor);
+        return result;
+    }
+    
+    public static long testExpr(IRNode irNode) {
+        IRExpr expr = (IRExpr) irNode;
+        IRStmt bBody = new IRSeq(new IRReturn(expr));
+        IRFuncDecl bFunc = new IRFuncDecl("function", bBody);
+        IRCompUnit compUnit = new IRCompUnit("test");
+        compUnit.appendFunc(bFunc);
+
+        IRSimulator sim = new IRSimulator(compUnit);
+        return sim.call("function");
+    }
+}
