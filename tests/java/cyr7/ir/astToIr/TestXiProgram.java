31df345948239e3846dd7a28da75073947783f1e
Anthony Yang <ay339@cornell.edu>
2020-03-08 19:58:36 -0400

    Test for prgm

---
+++ tests/java/cyr7/ir/astToIr/TestXiProgram.java
@@ -0,0 +1,64 @@
+package cyr7.ir.astToIr;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.io.StringReader;
+
+import org.junit.jupiter.api.Test;
+
+import cyr7.C;
+import cyr7.ast.Node;
+import cyr7.ast.toplevel.XiProgramNode;
+import cyr7.ir.AstToIrVisitor;
+import cyr7.ir.IdGenerator;
+import cyr7.ir.interpret.IRSimulator;
+import cyr7.ir.nodes.IRCompUnit;
+import cyr7.ir.nodes.IRFuncDecl;
+import cyr7.ir.nodes.IRNode;
+import cyr7.lexer.MultiFileLexer;
+import cyr7.parser.ParserUtil;
+import cyr7.parser.XiParser;
+import cyr7.typecheck.TypeCheckUtil;
+import java_cup.runtime.ComplexSymbolFactory;
+import java_cup.runtime.ScannerBuffer;
+
+class TestXiProgram {
+
+    private static void assertEq(IRNode expected, Node toTransform) {
+        assertEquals(expected,
+                toTransform.accept(new AstToIrVisitor()).assertFirst());
+    }
+
+    private static void assertEq(IRNode expected, Node toTransform,
+            IdGenerator generator) {
+        assertEquals(expected, toTransform.accept(new AstToIrVisitor(generator))
+                .assertFirst());
+    }
+
+    private static long test(IRNode expected, String prgm) throws Exception {
+        ScannerBuffer lexer = new ScannerBuffer(
+                new MultiFileLexer(new StringReader(prgm), "test.xi", false));
+        XiParser parser = new XiParser(lexer, new ComplexSymbolFactory());
+        XiProgramNode node = (XiProgramNode) parser.parse().value;
+        TypeCheckUtil.typecheck(node);
+        // Should be a statement
+        IRNode result = node.accept(new AstToIrVisitor()).assertSecond();
+        IRFuncDecl downcasted = (IRFuncDecl) result;
+        IRCompUnit compUnit = new IRCompUnit(C.LOC, "test");
+        compUnit.appendFunc(downcasted);
+
+        IRSimulator sim = new IRSimulator(compUnit);
+        return sim.call("main");
+    }
+
+
+    @Test
+    void test() {
+        String prgm = "main(): int[] { return 12; }";
+        ParserUtil.parse(new StringReader(prgm), writer, filename, isIXI);
+
+        fail("Not yet implemented");
+    }
+
+}
