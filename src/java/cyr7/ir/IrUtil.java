8c571632b6e8606778ed5e14abcb909b07063e87
Anthony Yang <ay339@cornell.edu>
2020-03-09 10:45:28 -0400

    Add block tests and set up blocks in IRUtil

--- src/java/cyr7/ir/IrUtil.java
+++ src/java/cyr7/ir/IrUtil.java
@@ -1,13 +1,17 @@
 package cyr7.ir;
 
+import java.io.PrintWriter;
+import java.io.Reader;
+import java.io.Writer;
+
 import cyr7.ast.Node;
+import cyr7.ir.block.TraceOptimizer;
 import cyr7.ir.interpret.IRSimulator;
 import cyr7.ir.lowering.LoweringVisitor;
 import cyr7.ir.nodes.IRCompUnit;
 import cyr7.ir.nodes.IRNode;
 import cyr7.ir.nodes.IRNodeFactory;
 import cyr7.ir.nodes.IRNodeFactory_c;
-import cyr7.ir.nodes.IRSeq;
 import cyr7.parser.ParserUtil;
 import cyr7.typecheck.IxiFileOpener;
 import cyr7.typecheck.TypeCheckUtil;
@@ -15,15 +19,11 @@ import edu.cornell.cs.cs4120.util.CodeWriterSExpPrinter;
 import edu.cornell.cs.cs4120.util.SExpPrinter;
 import java_cup.runtime.ComplexSymbolFactory.Location;
 
-import java.io.PrintWriter;
-import java.io.Reader;
-import java.io.Writer;
-
 public class IrUtil {
-    
+
     public static void mirRun(Reader reader, Writer writer, String filename,
                               boolean isIXI, IxiFileOpener opener) throws Exception {
-        
+
         Node result = ParserUtil.parseNode(reader, filename, isIXI);
         TypeCheckUtil.typeCheck(result, opener);
         IRCompUnit compUnit = (IRCompUnit) result.accept(new AstToIrVisitor())
@@ -37,7 +37,7 @@ public class IrUtil {
         long retVal = sim.call("_Imain_paai", 0);
         writer.append(String.valueOf(retVal)).append(System.lineSeparator());
     }
-    
+
     public static void irGen(Reader reader, Writer writer, String filename,
             boolean isIXI, IxiFileOpener fileOpener) throws Exception {
         IRNodeFactory make = new IRNodeFactory_c(new Location(1,1));
@@ -46,15 +46,18 @@ public class IrUtil {
         IdGenerator generator = new DefaultIdGenerator();
         IRNode node = result.accept(new AstToIrVisitor(generator))
                 .assertSecond();
-        IRSeq lowered = make.IRSeq(node.accept(
-                new LoweringVisitor(generator)).assertFirst()); 
-        
+        IRCompUnit lowered = node.accept(new LoweringVisitor(generator))
+                .assertThird();
+
         // TODO: Add optimization conditions
+        var trace = new TraceOptimizer(lowered, generator);
+        IRCompUnit optimized = trace.optimize();
+
         SExpPrinter printer = new CodeWriterSExpPrinter(new PrintWriter(writer));
-        lowered.printSExp(printer);
+        optimized.printSExp(printer);
         printer.flush();
     }
-    
+
     public static void irRun(Reader reader, Writer writer, String filename,
             boolean isIXI, IxiFileOpener fileOpener) throws Exception {
         Node result = ParserUtil.parseNode(reader, filename, isIXI);
@@ -63,12 +66,12 @@ public class IrUtil {
         IRNode node = result.accept(new AstToIrVisitor(generator))
                 .assertSecond();
         IRCompUnit lowered = node.accept(new LoweringVisitor(generator)).assertThird();
-        
+
         // TODO: Add optimization conditions
         IRSimulator sim = new IRSimulator(lowered);
         long retVal = sim.call("_Imain_paai", 0);
         writer.append(String.valueOf(retVal)).append(System.lineSeparator());
     }
-    
-    
+
+
 }
--- tests/java/cyr7/ir/block/TestBlockGeneration.java
+++ tests/java/cyr7/ir/block/TestBlockGeneration.java
@@ -3,12 +3,15 @@ package cyr7.ir.block;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 import org.junit.jupiter.api.Test;
 
+import cyr7.ir.DefaultIdGenerator;
+import cyr7.ir.IdGenerator;
 import cyr7.ir.nodes.IRBinOp.OpType;
 import cyr7.ir.nodes.IRNodeFactory;
 import cyr7.ir.nodes.IRNodeFactory_c;
@@ -18,16 +21,34 @@ import java_cup.runtime.ComplexSymbolFactory.Location;
 class TestBlockGeneration {
 
     private final Location loc = new Location(-1, -1);
+    private final IRNodeFactory make = new IRNodeFactory_c(loc);
+    private final IdGenerator generate = new DefaultIdGenerator();
 
-    void test(Set<BasicBlock> expected, BasicBlockSet actual) {
-        assertEquals(expected.size(), actual.size());
-        actual.forEach(b -> assertTrue(expected.contains(b)));
-        expected.forEach(b -> actual.contains(b));
+    void test(Set<BasicBlock> expected, List<IRStmt> actual) {
+        BasicBlockGenerator gen = new BasicBlockGenerator(actual);
+        BasicBlockSet blockSet = gen.getBlocks();
+        assertEquals(expected.size(), blockSet.size());
+        System.out.println(expected);
+        System.out.println();
+        System.out.println(blockSet);
+        assertEquals(expected, blockSet);
+        // Redundant here but just in case.
+        blockSet.forEach(b -> assertTrue(expected.contains(b)));
+        expected.forEach(b -> assertTrue(blockSet.contains(b)));
+        System.out.println();
+        System.out.println();
+    }
+
+    private Set<BasicBlock> createExpectedSet(BasicBlock... blocks) {
+        return new HashSet<>(Arrays.asList(blocks));
+    }
+
+    private BasicBlock block(IRStmt ...stmts) {
+        return new BasicBlock(Arrays.asList(stmts));
     }
 
     @Test
     void basicBlockTest() {
-        IRNodeFactory make = new IRNodeFactory_c(loc);
         String l1 = "_l1";
         String l2 = "_l2";
         List<IRStmt> stmts = List.of(
@@ -41,31 +62,22 @@ class TestBlockGeneration {
                 make.IRJump(make.IRName(l2))
         );
 
-        BasicBlockGenerator gen = new BasicBlockGenerator(stmts);
-        BasicBlockSet blockSet = gen.getBlocks();
-
-        Set<BasicBlock> expectedBlocks = new HashSet<>();
-        expectedBlocks.add(new BasicBlock(List.of(
-                make.IRLabel("main"),
-                make.IRJump(make.IRName(l1)
-        ))));
-        expectedBlocks.add(new BasicBlock(List.of(
-                make.IRLabel(l2),
-                make.IRMove(make.IRTemp("_t1"),
-                            make.IRBinOp(OpType.ADD, make.IRConst(1),
-                                                     make.IRConst(32)))
-        )));
-        expectedBlocks.add(new BasicBlock(List.of(
-                make.IRLabel(l1),
-                make.IRJump(make.IRName(l2))
-        )));
-
-        test(expectedBlocks, blockSet);
+        Set<BasicBlock> expectedBlocks = createExpectedSet(
+                block(
+                        make.IRLabel("main"),
+                        make.IRJump(make.IRName(l1))),
+                block(
+                        make.IRLabel(l2),
+                        make.IRMove(make.IRTemp("_t1"),
+                                    make.IRBinOp(OpType.ADD, make.IRConst(1),
+                                        make.IRConst(32)))),
+                block(make.IRLabel(l1), make.IRJump(make.IRName(l2)))
+        );
+        test(expectedBlocks, stmts);
     }
 
     @Test
     void eachStmtIsALabelTest() {
-        IRNodeFactory make = new IRNodeFactory_c(loc);
         List<IRStmt> stmts = List.of(
                 make.IRLabel("main"),
                 make.IRLabel("go"),
@@ -74,21 +86,113 @@ class TestBlockGeneration {
                 make.IRLabel("Ok_World")
                 );
 
-        BasicBlockGenerator gen = new BasicBlockGenerator(stmts);
-        BasicBlockSet blockSet = gen.getBlocks();
-
-        Set<BasicBlock> expectedBlocks = new HashSet<>();
-        expectedBlocks.add(new BasicBlock(List.of(make.IRLabel("main"))));
-        expectedBlocks.add(new BasicBlock(List.of(make.IRLabel("go"))));
-        expectedBlocks.add(new BasicBlock(List.of(make.IRLabel("truth"))));
-        expectedBlocks.add(new BasicBlock(List.of(make.IRLabel("compilers"))));
-        expectedBlocks.add(new BasicBlock(List.of(make.IRLabel("Ok_World"))));
-
-        test(expectedBlocks, blockSet);
+        Set<BasicBlock> expectedBlocks = createExpectedSet(
+                block(make.IRLabel("main")),
+                block(make.IRLabel("go")),
+                block(make.IRLabel("truth")),
+                block(make.IRLabel("compilers")),
+                block(make.IRLabel("Ok_World"))
+        );
+        test(expectedBlocks, stmts);
     }
 
     @Test
     void callDoesNotTerminateBlockTest() {
+        List<IRStmt> stmts = List.of(
+                make.IRLabel("main"),
+                make.IRReturn(),
 
+                make.IRLabel("go"),
+
+                make.IRLabel("truth"),
+                make.IRCallStmt(make.IRName("main")),
+                make.IRMove(make.IRTemp("t1"), make.IRConst(1232)),
+
+                make.IRLabel("compilers"),
+                make.IRMove(
+                        make.IRTemp(generate.argTemp(0)),
+                        make.IRConst(0)),
+                make.IRCallStmt(make.IRName("print"))
+        );
+
+        Set<BasicBlock> expectedBlocks = createExpectedSet(
+                block(make.IRLabel("main"), make.IRReturn()),
+
+                block(make.IRLabel("go")),
+
+                block(make.IRLabel("truth"),
+                        make.IRCallStmt(make.IRName("main")),
+                        make.IRMove(make.IRTemp("t1"), make.IRConst(1232))),
+
+                block(make.IRLabel("compilers"),
+                        make.IRMove(make.IRTemp(generate.argTemp(0)),
+                                make.IRConst(0)),
+                        make.IRCallStmt(make.IRName("print")))
+        );
+        test(expectedBlocks, stmts);
+    }
+
+
+    @Test
+    void onlyJumpsTest() {
+        List<IRStmt> stmts = List.of(
+                make.IRJump(make.IRName("hello")),
+                make.IRJump(make.IRName("hello")),
+                make.IRJump(make.IRName("hello")),
+                make.IRJump(make.IRName("hello")),
+                make.IRJump(make.IRName("hello"))
+        );
+
+        Set<BasicBlock> expectedBlocks = createExpectedSet(
+                block(make.IRJump(make.IRName("hello"))),
+                block(make.IRJump(make.IRName("hello"))),
+                block(make.IRJump(make.IRName("hello"))),
+                block(make.IRJump(make.IRName("hello"))),
+                block(make.IRJump(make.IRName("hello")))
+        );
+        test(expectedBlocks, stmts);
+
+
+        stmts = List.of(
+                make.IRJump(make.IRName("hello")),
+                make.IRJump(make.IRName("hello")),
+                make.IRLabel("main"),
+                make.IRJump(make.IRName("jello")),
+                make.IRJump(make.IRName("hello"))
+        );
+
+        expectedBlocks = createExpectedSet(
+                block(make.IRJump(make.IRName("hello"))),
+                block(make.IRJump(make.IRName("hello"))),
+                block(make.IRLabel("main"), make.IRJump(make.IRName("jello"))),
+                block(make.IRJump(make.IRName("hello")))
+        );
+        test(expectedBlocks, stmts);
+    }
+
+    @Test
+    void singleBlockTest() {
+        String t = generate.newTemp();
+        String r = generate.newTemp();
+        List<IRStmt> stmts = List.of(
+                make.IRLabel("main"),
+                make.IRMove(make.IRTemp(t), make.IRConst(123)),
+                make.IRCallStmt(make.IRName("randomString")),
+                make.IRMove(make.IRTemp(r), make.IRTemp(generate.retTemp(0))),
+                make.IRMove(make.IRTemp(generate.argTemp(0)), make.IRTemp(r)),
+                make.IRCallStmt(make.IRName("print")),
+                make.IRReturn()
+        );
+
+        Set<BasicBlock> expectedBlocks = createExpectedSet(
+           block(make.IRLabel("main"),
+                 make.IRMove(make.IRTemp(t), make.IRConst(123)),
+                 make.IRCallStmt(make.IRName("randomString")),
+                 make.IRMove(make.IRTemp(r), make.IRTemp(generate.retTemp(0))),
+                 make.IRMove(make.IRTemp(generate.argTemp(0)), make.IRTemp(r)),
+                 make.IRCallStmt(make.IRName("print")),
+                 make.IRReturn())
+        );
+        test(expectedBlocks, stmts);
     }
 }
