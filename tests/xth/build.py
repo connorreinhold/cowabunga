a6851c3753de8e76b08d231e18911bc1b09b1c85
Anthony Yang <ay339@cornell.edu>
2020-03-09 08:55:12 -0400

    Setup test harness. Runs instructor and our tests in tests/resources/irgen

--- tests/xth/build.py
+++ tests/xth/build.py
@@ -1,5 +1,6 @@
 import glob
 import os
+from pathlib import Path
 
 script_file = open(os.path.expanduser('~') + '/shared/cowabunga/tests/xth/xthScriptAll', 'w') 
 
@@ -7,7 +8,14 @@ def remove_extension(f):
     return os.path.splitext(f)[0]
 
 def add_xth_block(test_name, cli_arguments, search_pattern):
-    files_to_lex = sorted([remove_extension(f) for f in glob.glob(search_pattern, recursive=True)])
+    files_to_lex = []
+    for f in glob.glob(search_pattern, recursive=True):
+        dir_name = os.path.dirname(f)
+        file_name = os.path.basename(f)
+        index_of_dot = file_name.index('.')
+        file_name_without_extension = file_name[:index_of_dot]
+        files_to_lex.append(os.path.join(dir_name, file_name_without_extension))
+    # files_to_lex = sorted([remove_extension(f) for f in glob.glob(search_pattern, recursive=True)])
     script_file.write('xic ("' + test_name + '") "' + cli_arguments + '" {\n') 
 
     for f in files_to_lex:
@@ -25,12 +33,12 @@ if __name__ == '__main__':
     add_xth_block('Cowabunga Lexer Tests', '--lex', './shared/cowabunga/tests/**/*.lexedsol')
     add_xth_block('Cowabunga Parser Tests', '--parse', './shared/cowabunga/tests/**/*.parsedsol')
     add_xth_block('Cowabunga Type-checker Tests', '--typecheck -libpath ./shared/cowabunga/tests/resources/typecheck/lib', './shared/cowabunga/tests/**/*.typedsol')
-    add_xth_block('Cowabunga MIR Tests', '--mirrun', './shared/cowabunga/tests/**/*.irsol.nml')
+    add_xth_block('Cowabunga MIR Tests', '-libpath ./shared/cowabunga/tests/resources/irgen/lib --mirrun', './shared/cowabunga/tests/**/*.irsol.nml')
 
     add_xth_block('Instructor Lexer Tests', '--lex', './xth/tests/pa1/*.lexedsol')
     add_xth_block('Instructor Parser Tests', '--parse', './xth/tests/pa2/*.parsedsol')
     add_xth_block('Instructor Type-checker Tests', '-libpath ./xth/tests/pa3 --typecheck', './xth/tests/pa3/*.typedsol')
-    add_xth_block('Instructor MIR Tests', '--mirrun', './xth/tests/pa4/*.irsol.nml')
+    add_xth_block('Instructor MIR Tests', '-libpath ./xth/tests/pa4 --mirrun', './xth/tests/pa4/*.irsol.nml')
 
     script_file.close()
 
