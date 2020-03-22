b84e94c8bcd0624a6cc7475938b1311d1524abd4
Connor Reinhold <cyreinhold@gmail.com>
2020-03-21 20:00:00 -0400

    Fix for issue with open lock case

--- tests/java/cyr7/ir/integration/TestOpenTheLock.java
+++ tests/java/cyr7/ir/integration/TestOpenTheLock.java
@@ -1,5 +1,7 @@
 package cyr7.ir.integration;
 
+import cyr7.ir.integration.Run.RunConfiguration;
+
 public class TestOpenTheLock extends TestProgram {
 
     @Override
@@ -10,6 +12,11 @@ public class TestOpenTheLock extends TestProgram {
     @Override
     protected String expected() {
         return
-            "-1\n4\n3\n";
+            "-1\n4\n3\n8\n";
+    }
+
+    @Override
+    protected RunConfiguration configuration() {
+        return new RunConfiguration().bigHeap(true);
     }
 }
