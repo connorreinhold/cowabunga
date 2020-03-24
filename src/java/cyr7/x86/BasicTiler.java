8f64e2f0efafd01e5e2f88b33a6df05dfb4294e2
Connor Reinhold <cyreinhold@gmail.com>
2020-03-23 21:06:56 -0400

    Beginning work on BasicTiler

--- src/java/cyr7/x86/BasicTiler.java
+++ src/java/cyr7/x86/BasicTiler.java
@@ -2,93 +2,112 @@ package cyr7.x86;
 
 import cyr7.ir.nodes.*;
 import cyr7.visitor.MyIRVisitor;
+import cyr7.x86.asm.ASMArg;
+import cyr7.x86.asm.ASMFactory;
 import cyr7.x86.asm.ASMInstr;
+import cyr7.x86.TilerData;
+import cyr7.ir.IdGenerator;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
-public class BasicTiler implements MyIRVisitor<List<ASMInstr>> {
+public class BasicTiler implements MyIRVisitor<TilerData> {
+    
+    private final IdGenerator generator;
+    
+    public BasicTiler(IdGenerator generator) {
+        this.generator = generator;
+    }
+    
     @Override
-    public List<ASMInstr> visit(IRBinOp n) {
+    public TilerData visit(IRBinOp n) {
+        List<ASMInstr> insns = new ArrayList<ASMInstr>();
+        IRTemp ret = generator.newTemp();
+        
+        TilerData left = n.left().accept(this);
+        TilerData right = n.right().accept(this);
+        insns.add(ASMFactory.Mov(ret, left ));
         return null;
     }
 
     @Override
-    public List<ASMInstr> visit(IRCall n) {
+    public TilerData visit(IRCall n) {
         return null;
     }
 
     @Override
-    public List<ASMInstr> visit(IRConst n) {
+    public TilerData visit(IRConst n) {
         return null;
     }
 
     @Override
-    public List<ASMInstr> visit(IRESeq n) {
+    public TilerData visit(IRESeq n) {
         return null;
     }
 
     @Override
-    public List<ASMInstr> visit(IRMem n) {
+    public TilerData visit(IRMem n) {
         return null;
     }
 
     @Override
-    public List<ASMInstr> visit(IRName n) {
+    public TilerData visit(IRName n) {
         return null;
     }
 
     @Override
-    public List<ASMInstr> visit(IRTemp n) {
+    public TilerData visit(IRTemp n) {
         return null;
     }
 
     @Override
-    public List<ASMInstr> visit(IRCallStmt n) {
+    public TilerData visit(IRCallStmt n) {
         return null;
     }
 
     @Override
-    public List<ASMInstr> visit(IRCJump n) {
+    public TilerData visit(IRCJump n) {
         return null;
     }
 
     @Override
-    public List<ASMInstr> visit(IRCompUnit n) {
+    public TilerData visit(IRCompUnit n) {
         return null;
     }
 
     @Override
-    public List<ASMInstr> visit(IRExp n) {
+    public TilerData visit(IRExp n) {
         return null;
     }
 
     @Override
-    public List<ASMInstr> visit(IRFuncDecl n) {
+    public TilerData visit(IRFuncDecl n) {
         return null;
     }
 
     @Override
-    public List<ASMInstr> visit(IRJump n) {
+    public TilerData visit(IRJump n) {
         return null;
     }
 
     @Override
-    public List<ASMInstr> visit(IRLabel n) {
+    public TilerData visit(IRLabel n) {
         return null;
     }
 
     @Override
-    public List<ASMInstr> visit(IRMove n) {
+    public TilerData visit(IRMove n) {
         return null;
     }
 
     @Override
-    public List<ASMInstr> visit(IRReturn n) {
+    public TilerData visit(IRReturn n) {
         return null;
     }
 
     @Override
-    public List<ASMInstr> visit(IRSeq n) {
+    public TilerData visit(IRSeq n) {
         return null;
     }
 }
--- src/java/cyr7/x86/asm/ASMTempArg.java
+++ src/java/cyr7/x86/asm/ASMTempArg.java
@@ -1,4 +1,9 @@
 package cyr7.x86.asm;
 
 public class ASMTempArg extends ASMArg {
+    public final String name;
+    
+    public ASMTempArg(String name) {
+        this.name = name;
+    }
 }
