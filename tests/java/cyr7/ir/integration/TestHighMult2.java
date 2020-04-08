554f0a51f26f197d5e8dbd026cd88e72d312e263
Anthony Yang <ay339@cornell.edu>
2020-04-07 20:36:50 -0400

    Add test case for high multiplication

---
+++ tests/java/cyr7/ir/integration/TestHighMult2.java
@@ -0,0 +1,13 @@
+package cyr7.ir.integration;
+
+public class TestHighMult2 extends TestProgram {
+    @Override
+    protected String filename() {
+        return "highMult2";
+    }
+
+    @Override
+    protected String expected() {
+        return "-1\n";
+    }
+}
