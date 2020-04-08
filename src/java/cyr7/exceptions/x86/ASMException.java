00ffddf05c2fb9dc92e41a1e9f62eb4e05d5ef35
Anthony Yang <ay339@cornell.edu>
2020-04-08 06:41:17 -0400

    Fix issue where exception class was not correctly imported to basicTiler

--- src/java/cyr7/exceptions/x86/ASMException.java
+++ src/java/cyr7/exceptions/x86/ASMException.java
@@ -1,6 +1,11 @@
 package cyr7.exceptions.x86;
 
 public class ASMException extends RuntimeException {
+    /**
+     *
+     */
+    private static final long serialVersionUID = 5523715998716380058L;
+
     public ASMException(String s) {
         super(s);
     }
