0a77ee3d72d2ec0935f6131e2e0b93f2711a68f5
Orko Sinha <37596785+orkosinha@users.noreply.github.com>
2020-04-15 20:47:05 -0400

    Test cases for Temp + Temp and Const * Temp + Offset

---
+++ tests/java/cyr7/x86/tiler/TestTempPlusTemp.java
@@ -0,0 +1,22 @@
+package cyr7.x86.tiler;
+
+        import cyr7.ir.nodes.IRBinOp;
+        import org.junit.jupiter.api.Test;
+
+        import static cyr7.x86.tiler.ASMTestUtils.assertEqualsTiled;
+        import static cyr7.x86.tiler.ASMTestUtils.makeIR;
+
+public class TestTempPlusTemp {
+
+    @Test
+    void testTempAndTemp(){
+        IRBinOp tempTemp = makeIR(make ->
+                make.IRBinOp(IRBinOp.OpType.ADD,
+                        make.IRTemp("bleh1"),
+                        make.IRTemp("bleh2"))
+        );
+
+        assertEqualsTiled(tempTemp, "leaq _t0, [ bleh1 + 1 * bleh2 ]");
+    }
+
+}
