85d7e52f8a9680cbfc6d2eb3af425e3caa9c186a
William Ma <whoiswillma@gmail.com>
2020-04-17 20:55:43 -0400

    Add def and use checking to ASMTRAG

--- src/java/cyr7/x86/reg_allocator/ASMTrivialRegAllocGenerator.java
+++ src/java/cyr7/x86/reg_allocator/ASMTrivialRegAllocGenerator.java
@@ -1,23 +1,17 @@
 package cyr7.x86.reg_allocator;
 
-import cyr7.exceptions.x86.ASMException;
 import cyr7.ir.IdGenerator;
 import cyr7.ir.nodes.IRCompUnit;
 import cyr7.ir.nodes.IRFuncDecl;
 import cyr7.visitor.MyIRVisitor;
-import cyr7.x86.asm.ASMAddrExpr;
-import cyr7.x86.asm.ASMAddrExpr.ScaleValues;
-import cyr7.x86.asm.ASMArg;
 import cyr7.x86.asm.ASMArgFactory;
 import cyr7.x86.asm.ASMInstr;
 import cyr7.x86.asm.ASMLabel;
 import cyr7.x86.asm.ASMLine;
 import cyr7.x86.asm.ASMLineFactory;
-import cyr7.x86.asm.ASMMemArg;
 import cyr7.x86.asm.ASMReg;
 import cyr7.x86.asm.ASMTempArg;
 import cyr7.x86.asm.ASMTempArg.Size;
-import cyr7.x86.asm.ASMTempRegArg;
 import cyr7.x86.tiler.TilerData;
 import cyr7.x86.tiler.TilerFactory;
 import cyr7.x86.visitor.TempVisitor;
@@ -218,151 +212,13 @@ public class ASMTrivialRegAllocGenerator implements ASMGenerator {
         }).collect(Collectors.toUnmodifiableList());
     }
 
-    private List<ASMLine> replaceInstrTempArgsWithMemoryAccess(
+    protected List<ASMLine> replaceInstrTempArgsWithMemoryAccess(
         ASMInstr instr,
         Map<String, Integer> temporaryToIndexMap) {
 
-        TempToMemoryArgTranslator translator
-            = new TempToMemoryArgTranslator(temporaryToIndexMap);
+        ASMTrivialArgTranslator translator
+            = new ASMTrivialArgTranslator(temporaryToIndexMap);
         return translator.translate(instr);
-    }
-
-    private static final class TempToMemoryArgTranslator {
-
-        public static final ASMReg[] QWORD_REGS =
-            {ASMReg.R10, ASMReg.R11, ASMReg.R12, ASMReg.R13, ASMReg.R14,
-                ASMReg.R15};
-
-        public static final ASMReg[] BYTE_REGS =
-            {ASMReg.AL, ASMReg.BL, ASMReg.CL, ASMReg.DL};
-
-        private final List<ASMLine> prelude = new ArrayList<>();
-        private final List<ASMLine> postlude = new ArrayList<>();
-        private final List<ASMArg> newArgs = new ArrayList<>();
-
-        private int nextQwordReg = 0;
-        private int nextByteReg = 0;
-
-        private final Map<String, Integer> tempToIndexMap;
-
-        TempToMemoryArgTranslator(Map<String, Integer> tempToIndexMap) {
-            this.tempToIndexMap = tempToIndexMap;
-        }
-
-        List<ASMLine> translate(ASMInstr instr) {
-            for (ASMArg arg : instr.args) {
-                if (arg instanceof ASMMemArg) {
-                    ASMMemArg memArg = (ASMMemArg) arg;
-                    allocateRegisterForMemory(memArg);
-                } else if (arg instanceof ASMTempArg) {
-                    ASMTempArg tempArg = (ASMTempArg) arg;
-                    allocateMemoryForNormalTemporary(tempArg);
-                } else {
-                    newArgs.add(arg);
-                }
-            }
-
-            List<ASMLine> lines = new ArrayList<>();
-            lines.addAll(prelude);
-            lines.add(instr.withArgsReplaced(newArgs));
-            lines.addAll(postlude);
-            return lines;
-        }
-
-        // allocate registers for temporaries inside a mem arg
-        private void allocateRegisterForMemory(ASMMemArg arg) {
-            Optional<ASMTempRegArg> base = Optional.empty();
-            Optional<ASMTempRegArg> index = Optional.empty();
-
-            if (arg.address.base.isPresent()) {
-                ASMTempRegArg b = arg.address.base.get();
-                base = Optional.of(allocateRegisterForMemoryOperand(b));
-            }
-            if (arg.address.index.isPresent()) {
-                ASMTempRegArg i = arg.address.index.get();
-                index = Optional.of(allocateRegisterForMemoryOperand(i));
-            }
-
-            ASMAddrExpr address = new ASMAddrExpr(
-                base,
-                arg.address.scale,
-                index,
-                arg.address.displacement);
-            newArgs.add(new ASMMemArg(address));
-        }
-
-        // allocate registers for temporaries that are regular arguments to an
-        // asm instruction (i.e. not inside memory access)
-        private void allocateMemoryForNormalTemporary(ASMTempArg arg) {
-            switch (arg.size) {
-                case BYTE: {
-                    ASMReg reg = BYTE_REGS[nextByteReg++];
-                    ASMReg qwordReg = reg.correspondingQWordReg();
-
-                    prelude.add(make.Push(qwordReg));
-                    prelude.add(make.Mov(qwordReg,
-                        new ASMMemArg(addressOfTemporary(arg))));
-
-                    postlude.add(0, make.Pop(qwordReg));
-                    postlude.add(0,
-                        make.Mov(new ASMMemArg(addressOfTemporary(arg)), qwordReg));
-
-                    newArgs.add(reg);
-
-                    break;
-                }
-                case QWORD: {
-                    ASMReg reg = QWORD_REGS[nextQwordReg++];
-
-                    prelude.add(make.Mov(reg,
-                        new ASMMemArg(addressOfTemporary(arg))));
-
-                    postlude.add(0,
-                        make.Mov(new ASMMemArg(addressOfTemporary(arg)), reg));
-
-                    newArgs.add(reg);
-
-                    break;
-                }
-            }
-        }
-
-        private ASMTempRegArg allocateRegisterForMemoryOperand(ASMTempRegArg arg) {
-            if (arg instanceof ASMReg) {
-                return arg;
-            } else if (arg instanceof ASMTempArg) {
-                ASMTempArg tempArg = (ASMTempArg) arg;
-                ASMReg reg = QWORD_REGS[nextQwordReg];
-
-                ASMAddrExpr address = addressOfTemporary(tempArg);
-                prelude.add(make.Mov(reg, new ASMMemArg(address)));
-                postlude.add(0, make.Mov(new ASMMemArg(address), reg));
-                nextQwordReg++;
-                return reg;
-            } else {
-                throw new ASMException("ASMTempRegArg " + arg + " is neither " +
-                    "a temporary nor a register.");
-            }
-
-        }
-
-        private ASMAddrExpr addressOfTemporary(ASMTempArg tempArg) {
-            Integer index = tempToIndexMap.get(tempArg.name);
-            if (index == null) {
-                throw new ASMException(
-                    "Could not find index for temporary \""
-                        + tempArg.name
-                        + "\"");
-            }
-
-            // [RBP + 8 * (index + 1)]
-            return arg.addr(
-                Optional.of(ASMReg.RBP),
-                ScaleValues.ONE,
-                Optional.empty(),
-                -8 * (index + 1));
-        }
-
     }
 
 }
