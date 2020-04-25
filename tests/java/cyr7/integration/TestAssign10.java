ad725d1a3866b90997e270c62c9300eb97eeb88e
Anthony Yang <ay339@cornell.edu>
2020-04-24 23:19:23 -0400

    Fix issues with local declarations, i.e. size expression evaluated multiple times. Fix test cases that had incorrect expected and filename values

--- tests/java/cyr7/integration/TestAssign10.java
+++ tests/java/cyr7/integration/TestAssign10.java
@@ -2,7 +2,7 @@ package cyr7.integration;
 
 public class TestAssign10 extends TestProgram {
     @Override
-    protected String filename() {
+    protected String expected() {
         return "Index: 4 2\n" +
                 "Index: 4 2\n" +
                 "0\n" +
@@ -28,7 +28,7 @@ public class TestAssign10 extends TestProgram {
     }
 
     @Override
-    protected String expected() {
-        return null;
+    protected String filename() {
+        return "assign10";
     }
 }
