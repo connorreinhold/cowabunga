fdc0d64cfede67eb212cd04a57b25e4579b28164
William Ma <whoiswillma@gmail.com>
2020-04-30 22:40:07 -0400

    Update TestLiveVariableAnalysis.java

--- tests/java/cyr7/cfg/TestLiveVariableAnalysis.java
+++ tests/java/cyr7/cfg/TestLiveVariableAnalysis.java
@@ -3,8 +3,8 @@ package cyr7.cfg;
 import cyr7.C;
 import cyr7.cfg.dfa.LiveVariableAnalysis;
 import cyr7.cfg.dfa.WorklistAnalysis;
-import cyr7.cfg.nodes.CFGNode;
 import cyr7.cfg.nodes.CFGNodeFactory;
+import cyr7.cfg.nodes.CFGReturnNode;
 import cyr7.cfg.nodes.CFGStartNode;
 import cyr7.cfg.nodes.CFGVarAssignNode;
 import cyr7.ir.interpret.Configuration;
@@ -13,6 +13,11 @@ import cyr7.ir.nodes.IRNodeFactory;
 import cyr7.ir.nodes.IRNodeFactory_c;
 import org.junit.jupiter.api.Test;
 
+import java.util.Map;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TestLiveVariableAnalysis {
 
     private static final CFGNodeFactory make = new CFGNodeFactory(C.LOC);
@@ -20,15 +25,40 @@ public class TestLiveVariableAnalysis {
 
     @Test
     void testLectureExample() {
-        CFGStartNode node = make.Start(
-            make.VarAssign("a", makeIR.IRBinOp(OpType.ADD, makeIR.IRTemp("a"), makeIR.IRConst(2)),
-                make.VarAssign("c", makeIR.IRBinOp(OpType.MUL, makeIR.IRTemp("b"), makeIR.IRTemp("b")),
-                    make.VarAssign(Configuration.ABSTRACT_RET_PREFIX + "0", makeIR.IRBinOp(OpType.ADD, makeIR.IRTemp("c"), makeIR.IRTemp("e")),
-                        make.Return(1))))
-        );
+        // 1: int b = a + 2;
+        // 2: int c = b * b;
+        // 3: int d = c + e;
+        // 4: return d;
 
-        var result = WorklistAnalysis.analyze(node, LiveVariableAnalysis.INSTANCE);
-        System.out.println(result);
+        CFGReturnNode line4 = make.Return(1);
+        CFGVarAssignNode line3 = make.VarAssign(
+            Configuration.ABSTRACT_RET_PREFIX + "0",
+            makeIR.IRBinOp(OpType.ADD,
+                makeIR.IRTemp("c"),
+                makeIR.IRTemp("e")),
+            line4);
+        CFGVarAssignNode line2 = make.VarAssign(
+            "c",
+            makeIR.IRBinOp(OpType.MUL,
+                makeIR.IRTemp("b"),
+                makeIR.IRTemp("b")),
+            line3);
+        CFGVarAssignNode line1 = make.VarAssign(
+            "b",
+            makeIR.IRBinOp(OpType.ADD,
+                makeIR.IRTemp("a"),
+                makeIR.IRConst(2)),
+            line2);
+        CFGStartNode line0 = make.Start(line1);
+
+        var result = WorklistAnalysis.analyze(line0, LiveVariableAnalysis.INSTANCE);
+        assertEquals(Map.of(
+            line0, Set.of(),
+            line1, Set.of("a"),
+            line2, Set.of("b"),
+            line3, Set.of("c", "e"),
+            line4, Set.of("_RET0")),
+            result);
     }
 
 }
