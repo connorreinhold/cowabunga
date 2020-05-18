cc9628f511b9d19d9b11895ea89f97c5be1b4f1b
William Ma <whoiswillma@gmail.com>
2020-05-17 22:24:42 -0400

    Change benchmark suite to only enable tested optimization

--- tests/java/cyr7/benchmark/Benchmark.java
+++ tests/java/cyr7/benchmark/Benchmark.java
@@ -6,7 +6,6 @@ import cyr7.cli.Optimization;
 import cyr7.x86.ASMUtil.TilerConf;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Timeout;
 import org.junit.jupiter.api.condition.EnabledOnOs;
 import org.junit.jupiter.api.condition.OS;
 
@@ -23,6 +22,11 @@ public abstract class Benchmark {
 
     abstract Optimization[] testedOptimizations();
 
+    int repetitions() {
+        // override this if you want to change the number of reps
+        return 5;
+    }
+
     private String getTestAssemblyFilename() {
         return "tests/resources/" + filename() + ".s";
     }
@@ -31,10 +35,16 @@ public abstract class Benchmark {
         return "tests/resources/" + filename() + ".xi";
     }
 
-    private long runBenchmark(OptConfig optConfig, int repetitions) throws Exception {
-        Bash.compileToAssembly(getTestXiFilename(), TilerConf.COMPLEX, optConfig);
+    private long runBenchmark(OptConfig optConfig)
+        throws Exception {
+
+        Bash.compileToAssembly(
+            getTestXiFilename(),
+            TilerConf.COMPLEX,
+            optConfig);
         File exe = Bash.linkExecutable(getTestAssemblyFilename());
 
+        int repetitions = repetitions();
         long totalTime = 0;
         for (int i = 0; i < repetitions; i++) {
             Instant start = Instant.now();
@@ -48,16 +58,14 @@ public abstract class Benchmark {
     @EnabledOnOs({OS.LINUX})
     @Test
     void benchmark() throws Exception {
-        OptConfig unoptimizedOptConfig = OptConfig.allEnabled();
-        for (Optimization opt : testedOptimizations()) {
-            unoptimizedOptConfig.set(opt, false);
-        }
-
-        long unoptimizedMillis = runBenchmark(unoptimizedOptConfig, 10);
-        long optimizedMillis = runBenchmark(OptConfig.allEnabled(), 10);
+        long unoptimizedMillis
+            = runBenchmark(OptConfig.none());
+        long optimizedMillis
+            = runBenchmark(OptConfig.of(testedOptimizations()));
         System.out.println("Unoptimized: " + unoptimizedMillis);
         System.out.println("Optimized: " + optimizedMillis);
-        assertTrue(100 < optimizedMillis && optimizedMillis < 10000, "Benchmark test cases must execute between 1s and 3s");
+        assertTrue(100 < optimizedMillis && optimizedMillis < 10000,
+            "Benchmark test cases must execute between 1s and 3s");
         assertTrue(optimizedMillis < unoptimizedMillis,
             "Unoptimized version was faster than optimized version.");
     }
---
+++ tests/resources/benchmarks/reg/reg_benchmark3.xi
@@ -0,0 +1,10 @@
+use conv
+use io
+main(args: int[][]) {
+    i: int = 0
+    x: int[] = ""
+    while i < 5000 {
+        x = x + "Hello"
+        i = i + 1
+    }
+}
